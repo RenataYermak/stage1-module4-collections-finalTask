a2fae44d989f5d935ebb58a26620ffa7a217d611
Renata Yermak <renatkahturo@gmail.com>
2023-04-12 11:39:41 +0300

    .

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,29 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> result = new ArrayList<>();
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            if (entry.getValue().contains(developer)) {
+                result.add(entry.getKey());
+            }
+        }
+        result.sort(new StringComparator());
+        return result;
+    }
+
+    private static class StringComparator implements Comparator<String> {
+        public int compare(String a, String b) {
+            if (a.length() != b.length())
+                return b.length() - a.length();
+            else
+                return b.compareTo(a);
+        }
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,22 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> result = new HashMap<>();
+        for (Map.Entry<String, Integer> element : sourceMap.entrySet()) {
+            if (result.containsKey(element.getKey().length())) {
+                result.get(element.getKey().length()).add(element.getKey());
+            } else {
+                Set<String> set = new HashSet<>();
+                set.add(element.getKey());
+                result.put(element.getKey().length(), set);
+            }
+        }
+        return result;
     }
 }
